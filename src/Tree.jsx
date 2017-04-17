01b511b0c8a8510f581ef4973d3117b576b06758
afc163 <afc163@gmail.com>
2017-04-17 12:09:23 +0800

    fix PropTypes warning

--- src/Tree.jsx
+++ src/Tree.jsx
@@ -1,5 +1,6 @@
 /* eslint no-console:0 */
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import assign from 'object-assign';
 import classNames from 'classnames';
 import {
--- src/TreeNode.jsx
+++ src/TreeNode.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import assign from 'object-assign';
 import classNames from 'classnames';
 import Animate from 'rc-animate';
