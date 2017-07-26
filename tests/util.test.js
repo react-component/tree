eb039947ec0eefd7241874e31f4b533768e36dac
Nathan Bierema <nbierema@gmail.com>
2017-07-26 08:10:53 -0400

    Update util.test.js

--- tests/util.test.js
+++ tests/util.test.js
@@ -8,6 +8,6 @@ test('[0, 1] equals [0, 1]', () => {
 
 test('[0, 1] does not equal [1, 0]', () => {
   const array1 = [0, 1];
-  const array2 = [0, 1];
-  expect(arraysEqual(array2, array1)).toBe(true);
+  const array2 = [1, 0];
+  expect(arraysEqual(array2, array1)).toBe(false);
 });
