0ff6d97d0404d08261d8f9fecff4bb4e929baa47
jiljiang <jiljiang@ebay.com>
2018-03-03 14:36:50 +0800

    selectable test

--- tests/TreeProps.spec.js
+++ tests/TreeProps.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 import { renderToJson } from 'enzyme-to-json';
 import Tree, { TreeNode } from '..';
 
@@ -70,5 +70,62 @@ describe('Tree Props', () => {
       </Tree>
     );
     expect(renderToJson(withOpenIcon)).toMatchSnapshot();
+  });
+
+  // selectable
+  it('selectable', () => {
+    // With Selectable
+    const handleOnSelect = jest.fn();
+    const withSelectableBase = (
+      <Tree onSelect={handleOnSelect} defaultExpandedKeys={['0-0']}>
+        <TreeNode key="0-0">
+          <TreeNode key="0-0-0" />
+        </TreeNode>
+      </Tree>
+    );
+
+    expect(renderToJson(render(withSelectableBase))).toMatchSnapshot();
+
+    const withSelectable = mount(withSelectableBase);
+    const parentNode = withSelectable.find(TreeNode).first();
+    const targetNode = parentNode.find(TreeNode).last();
+
+    // Check leaf
+    targetNode.find('.rc-tree-node-content-wrapper').simulate('click');
+
+    // traverseTreeNodes loops origin TreeNode and
+    // onSelect trigger on `cloneElement` which is not the same instance
+    expect(handleOnSelect).toBeCalledWith(['0-0-0'], {
+      event: 'select',
+      selected: true,
+      node: targetNode.instance(),
+      selectedNodes: [parentNode.props().children],
+    });
+
+    // Uncheck leaf
+    targetNode.find('.rc-tree-node-content-wrapper').simulate('click');
+    expect(handleOnSelect).toBeCalledWith([], {
+      event: 'select',
+      selected: false,
+      node: targetNode.instance(),
+      selectedNodes: [],
+    });
+
+    // Check leaf with parent
+    targetNode.find('.rc-tree-node-content-wrapper').simulate('click');
+    expect(handleOnSelect).toBeCalledWith(['0-0-0'], {
+      event: 'select',
+      selected: true,
+      node: targetNode.instance(),
+      selectedNodes: [parentNode.props().children],
+    });
+
+    parentNode.find('.rc-tree-node-content-wrapper').first().simulate('click');
+    expect(handleOnSelect).toBeCalledWith(['0-0'], {
+      event: 'select',
+      selected: true,
+      node: parentNode.instance(),
+      selectedNodes: [withSelectable.find(Tree).props().children],
+    });
   });
 });
