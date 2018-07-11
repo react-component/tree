d1c8d0102d2d83549907dae26a34380cb4f46231
zombiej <smith3816@gmail.com>
2018-07-11 16:27:32 +0800

    add disabled check test case

--- src/util.js
+++ src/util.js
@@ -415,6 +415,7 @@ export function convertDataToTree(treeData) {
   });
 }
 
+// TODO: ========================= NEW LOGIC =========================
 /**
  * Calculate treeNodes entities.
  * @param treeNodes
@@ -448,4 +449,125 @@ export function convertTreeToEntities(treeNodes, processTreeEntity) {
   });
 
   return wrapper;
+}
+
+export function conductCheck(keyList, isCheck, keyEntities, checkStatus = {}) {
+  const checkedKeys = {};
+  const halfCheckedKeys = {}; // Record the key has some child checked (include child half checked)
+
+  (checkStatus.checkedKeys || []).forEach((key) => {
+    checkedKeys[key] = true;
+  });
+
+  (checkStatus.halfCheckedKeys || []).forEach((key) => {
+    halfCheckedKeys[key] = true;
+  });
+
+  // Conduct up
+  function conductUp(key) {
+    if (checkedKeys[key] === isCheck) return;
+
+    const entity = keyEntities[key];
+    if (!entity) return;
+
+    const { children, parent, node } = entity;
+
+    if (isCheckDisabled(node)) return;
+
+    // Check child node checked status
+    let everyChildChecked = true;
+    let someChildChecked = false; // Child checked or half checked
+
+    (children || [])
+      .filter(child => !isCheckDisabled(child.node))
+      .forEach(({ key: childKey }) => {
+        const childChecked = checkedKeys[childKey];
+        const childHalfChecked = halfCheckedKeys[childKey];
+
+        if (childChecked || childHalfChecked) someChildChecked = true;
+        if (!childChecked) everyChildChecked = false;
+      });
+
+    // Update checked status
+    if (isCheck) {
+      checkedKeys[key] = everyChildChecked;
+    } else {
+      checkedKeys[key] = false;
+    }
+    halfCheckedKeys[key] = someChildChecked;
+
+    if (parent) {
+      conductUp(parent.key);
+    }
+  }
+
+  // Conduct down
+  function conductDown(key) {
+    if (checkedKeys[key] === isCheck) return;
+
+    const entity = keyEntities[key];
+    if (!entity) return;
+
+    const { children, node } = entity;
+
+    if (isCheckDisabled(node)) return;
+
+    checkedKeys[key] = isCheck;
+
+    (children || []).forEach((child) => {
+      conductDown(child.key);
+    });
+  }
+
+  function conduct(key) {
+    const entity = keyEntities[key];
+
+    if (!entity) {
+      warning(false, `'${key}' does not exist in the tree.`);
+      return;
+    }
+
+    const { children, parent, node } = entity;
+    checkedKeys[key] = isCheck;
+
+    if (isCheckDisabled(node)) return;
+
+    // Conduct down
+    (children || [])
+      .filter(child => !isCheckDisabled(child.node))
+      .forEach((child) => {
+        conductDown(child.key);
+      });
+
+    // Conduct up
+    if (parent) {
+      conductUp(parent.key);
+    }
+  }
+
+  (keyList || []).forEach((key) => {
+    conduct(key);
+  });
+
+  const checkedKeyList = [];
+  const halfCheckedKeyList = [];
+
+  // Fill checked list
+  Object.keys(checkedKeys).forEach((key) => {
+    if (checkedKeys[key]) {
+      checkedKeyList.push(key);
+    }
+  });
+
+  // Fill half checked list
+  Object.keys(halfCheckedKeys).forEach((key) => {
+    if (!checkedKeys[key] && halfCheckedKeys[key]) {
+      halfCheckedKeyList.push(key);
+    }
+  });
+
+  return {
+    checkedKeys: checkedKeyList,
+    halfCheckedKeys: halfCheckedKeyList,
+  };
 }
--- tests/util.spec.js
+++ tests/util.spec.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Tree from '../src/Tree';
 import TreeNode from '../src/TreeNode';
-import { convertDataToTree } from '../src/util';
+import { convertDataToTree, convertTreeToEntities, conductCheck } from '../src/util';
 import { convertTreeToData } from './util';
 
 describe('Util', () => {
@@ -62,5 +62,106 @@ describe('Util', () => {
     const $treeNode = convertDataToTree(treeData);
 
     expect(convertTreeToData($treeNode)).toEqual(treeData);
+  });
+
+  // You can remove this test if refactor remove conductCheck function
+  describe('conductCheck', () => {
+    describe('basic', () => {
+      const genTree = (props) => (
+        <Tree {...props}>
+          <TreeNode key="greed" title="GreeIs" />
+          <TreeNode key="good" title="good">
+            <TreeNode key="there" title="There" />
+            <TreeNode key="is" title="Is">
+              <TreeNode key="no" title="No" />
+              <TreeNode key="spoon" title="Spoon">
+                <TreeNode key="i" title="I" />
+                <TreeNode key="see" title="See" />
+                <TreeNode key="dead" title="Dead">
+                  <TreeNode key="people" title="People" />
+                  <TreeNode key="!" title="!" />
+                </TreeNode>
+              </TreeNode>
+            </TreeNode>
+          </TreeNode>
+        </Tree>
+      );
+
+      it('check', () => {
+        const tree = genTree();
+        const { keyEntities } = convertTreeToEntities(tree.props.children);
+        const { checkedKeys, halfCheckedKeys } =
+          conductCheck(['spoon'], true, keyEntities);
+        expect(checkedKeys.sort()).toEqual(['spoon', 'i', 'see', 'dead', 'people', '!'].sort());
+        expect(halfCheckedKeys.sort()).toEqual(['good', 'is'].sort());
+      });
+
+      it('uncheck', () => {
+        const tree = genTree({ checkedKeys: ['greed', 'good'] });
+        const { keyEntities } = convertTreeToEntities(tree.props.children);
+
+        // First, we check all
+        const allCheckedKeys = conductCheck(['greed', 'good'], true, keyEntities).checkedKeys;
+        expect(allCheckedKeys.length).toEqual(11);
+
+        // Then uncheck one of then
+        const { checkedKeys, halfCheckedKeys } =
+          conductCheck(['spoon'], false, keyEntities, { checkedKeys: allCheckedKeys });
+        expect(checkedKeys.sort()).toEqual(['greed', 'there', 'no'].sort());
+        expect(halfCheckedKeys.sort()).toEqual(['good', 'is'].sort());
+      });
+    });
+
+    describe('part disabled', () => {
+      const genTree = (props) => (
+        <Tree {...props}>
+          <TreeNode key="war">
+            <TreeNode key="are" />
+            <TreeNode key="not">
+              <TreeNode key="it" />
+              <TreeNode key="used" disabled>
+                <TreeNode key="to" />
+                <TreeNode key="be" />
+              </TreeNode>
+            </TreeNode>
+          </TreeNode>
+        </Tree>
+      );
+
+      it('check', () => {
+        const tree = genTree();
+        const { keyEntities } = convertTreeToEntities(tree.props.children);
+
+        const result1 =
+          conductCheck(['not'], true, keyEntities);
+        expect(result1.checkedKeys.sort()).toEqual(['not', 'it'].sort());
+        expect(result1.halfCheckedKeys.sort()).toEqual(['war'].sort());
+
+        const result2 =
+          conductCheck(['to', 'be'], true, keyEntities);
+        expect(result2.checkedKeys.sort()).toEqual(['to', 'be'].sort());
+        expect(result2.halfCheckedKeys.sort()).toEqual([].sort());
+      });
+
+      it('uncheck', () => {
+        const tree = genTree();
+        const { keyEntities } = convertTreeToEntities(tree.props.children);
+
+        // First, we check all
+        const allCheckedKeys = conductCheck(['war', 'to', 'be'], true, keyEntities).checkedKeys;
+        expect(allCheckedKeys.length).toEqual(6);
+
+        // Then uncheck one of then
+        const result1 =
+          conductCheck(['not'], false, keyEntities, { checkedKeys: allCheckedKeys });
+        expect(result1.checkedKeys.sort()).toEqual(['are', 'to', 'be'].sort());
+        expect(result1.halfCheckedKeys.sort()).toEqual(['war'].sort());
+
+        const result2 =
+          conductCheck(['to', 'be'], false, keyEntities, { checkedKeys: allCheckedKeys });
+        expect(result2.checkedKeys.sort()).toEqual(['war', 'are', 'not', 'it'].sort());
+        expect(result2.halfCheckedKeys.sort()).toEqual([].sort());
+      });
+    });
   });
 });
