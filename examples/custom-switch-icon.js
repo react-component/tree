d1058b391476e5504c6ed6748f034ffa00d9e8da
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-08-08 16:47:04 +0800

    reduce example

--- examples/custom-switch-icon.js
+++ examples/custom-switch-icon.js
@@ -6,28 +6,6 @@ import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import Tree, { TreeNode } from 'rc-tree';
 import 'rc-tree/assets/index.less';
-import './basic.less';
-
-const treeData = [
-  {
-    key: '0-0', title: 'parent 1', children:
-      [
-        {
-          key: '0-0-0', title: 'parent 1-1', children:
-            [
-              { key: '0-0-0-0', title: 'parent 1-1-0' },
-            ],
-        },
-        {
-          key: '0-0-1', title: 'parent 1-2', children:
-            [
-              { key: '0-0-1-0', title: 'parent 1-2-0', disableCheckbox: true },
-              { key: '0-0-1-1', title: 'parent 1-2-1' },
-            ],
-        },
-      ],
-  },
-];
 
 const arrowPath = 'M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88' +
   '.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.' +
@@ -66,40 +44,7 @@ class Demo extends React.Component {
       defaultCheckedKeys: keys,
     };
   }
-  onExpand = (expandedKeys) => {
-    console.log('onExpand', expandedKeys, arguments);
-  };
-  onSelect = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info);
-    this.selKey = info.node.props.eventKey;
-  };
-  onCheck = (checkedKeys, info) => {
-    console.log('onCheck', checkedKeys, info);
-  };
-  onEdit = () => {
-    setTimeout(() => {
-      console.log('current key: ', this.selKey);
-    }, 0);
-  };
-  onDel = (e) => {
-    if (!window.confirm('sure to delete?')) {
-      return;
-    }
-    e.stopPropagation();
-  };
   render() {
-    const customLabel = (
-      <span className="cus-label">
-        <span>operations: </span>
-        <span style={{ color: 'blue' }} onClick={this.onEdit}>Edit</span>&nbsp;
-        <label onClick={(e) => e.stopPropagation()}>
-          <input type="checkbox" /> checked
-        </label>
-        &nbsp;
-        <span style={{ color: '#EB0000' }} onClick={this.onDel}>Delete</span>
-      </span>
-    );
-
     const switcherIcon = (obj) => {
       if (obj.isLeaf) {
         return getSvgIcon(arrowPath,
@@ -114,19 +59,17 @@ class Demo extends React.Component {
 
     return (
       <div id="demo" style={{ margin: '0 20px' }}>
-        <h2>simple</h2>
+        <h2>custom switch icon</h2>
         <Tree
           className={treeCls} showLine checkable defaultExpandAll
           defaultExpandedKeys={this.state.defaultExpandedKeys}
-          onExpand={this.onExpand}
           defaultSelectedKeys={this.state.defaultSelectedKeys}
           defaultCheckedKeys={this.state.defaultCheckedKeys}
-          onSelect={this.onSelect} onCheck={this.onCheck}
           switcherIcon={switcherIcon}
         >
           <TreeNode title="parent 1" key="0-0">
-            <TreeNode title={customLabel} key="0-0-0">
-              <TreeNode title="leaf" key="0-0-0-0" style={{ background: 'rgba(255, 0, 0, 0.1)' }} />
+            <TreeNode title="leaf" key="0-0-0">
+              <TreeNode title="leaf" key="0-0-0-0" />
               <TreeNode title="leaf" key="0-0-0-1" />
             </TreeNode>
             <TreeNode title="parent 1-1" key="0-0-1">
@@ -137,24 +80,12 @@ class Demo extends React.Component {
               <TreeNode title="parent 1-2-0" key="0-0-2-0" disabled />
               <TreeNode title="parent 1-2-1" key="0-0-2-1" />
             </TreeNode>
+            <TreeNode title="parent 1-3" key="0-0-3">
+              <TreeNode title="parent 1-3-0" key="0-0-3-0" />
+              <TreeNode title="parent 1-3-1" key="0-0-3-1" />
+            </TreeNode>
           </TreeNode>
         </Tree>
-
-        <h2>Check on Click TreeNode</h2>
-        <Tree
-          className={treeCls}
-          showLine
-          checkable
-          selectable={false}
-          defaultExpandAll
-          onExpand={this.onExpand}
-          defaultSelectedKeys={this.state.defaultSelectedKeys}
-          defaultCheckedKeys={this.state.defaultCheckedKeys}
-          onSelect={this.onSelect}
-          onCheck={this.onCheck}
-          treeData={treeData}
-          switcherIcon={switcherIcon}
-        />
       </div>
     );
   }
