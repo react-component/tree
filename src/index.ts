66d9976805d32e381e912fd04c21cf4b0012f3bc
zombiej <smith3816@gmail.com>
2019-07-24 16:40:06 +0800

    index ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import Tree from './Tree';
+import OriginTree from './Tree';
 import TreeNode from './TreeNode';
 
+type OriginTreeType = typeof OriginTree;
+interface TreeType extends OriginTreeType {
+  TreeNode: typeof TreeNode;
+}
+
+const Tree = OriginTree as TreeType;
 Tree.TreeNode = TreeNode;
 
 export { TreeNode };
