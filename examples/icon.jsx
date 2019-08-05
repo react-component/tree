1e11148247063d3cdc0155e9dc0f9f367654327c
zombiej <smith3816@gmail.com>
2019-08-05 15:14:13 +0800

    fix icon demo

--- examples/icon.jsx
+++ examples/icon.jsx
@@ -18,22 +18,22 @@ const Demo = () => (
   <div>
     <h2>Customize icon with element</h2>
     <Tree defaultExpandAll>
-      <TreeNode icon={<span className="customize-icon" />} title="Parent">
-        <TreeNode icon={<span className="customize-icon sub-icon" />} title="Child" />
+      <TreeNode icon={<span className="customize-icon" />} title="Parent" key="0">
+        <TreeNode icon={<span className="customize-icon sub-icon" />} title="Child" key="00" />
       </TreeNode>
     </Tree>
 
     <h2>Customize icon with component</h2>
     <Tree defaultExpandAll>
-      <TreeNode icon={Icon} title="Parent">
-        <TreeNode icon={Icon} title="Child" />
+      <TreeNode icon={Icon} title="Parent" key="0">
+        <TreeNode icon={Icon} title="Child" key="00" />
       </TreeNode>
     </Tree>
 
     <h2>Customize icon with Tree prop</h2>
     <Tree defaultExpandAll icon={Icon}>
-      <TreeNode title="Parent">
-        <TreeNode title="Child" />
+      <TreeNode title="Parent" key="0">
+        <TreeNode title="Child" key="00" />
       </TreeNode>
     </Tree>
   </div>
