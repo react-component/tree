bf97f10517be0ae60614b765aa4c09c5aa49a0c2
zombiej <smith3816@gmail.com>
2019-08-09 16:57:09 +0800

    update demo

--- examples/animation-draggable.jsx
+++ examples/animation-draggable.jsx
@@ -1,6 +1,7 @@
-/* eslint-disable no-console, react/no-access-state-in-setstate, react/no-danger */
+/* eslint-disable no-console, react/no-access-state-in-setstate,
+react/no-danger, no-param-reassign */
 import React from 'react';
-// import { gData } from './utils/dataUtil';
+import { gData } from './utils/dataUtil';
 import '../assets/index.less';
 import Tree from '../src';
 
@@ -27,18 +28,17 @@ const motion = {
   onLeaveActive: () => ({ height: 0 }),
 };
 
-const gData = [
-  { title: '0-0', key: '0-0' },
-  { title: '0-1', key: '0-1' },
-  { title: '0-2', key: '0-2', children: [{ title: '0-2-0', key: '0-2-0' }] },
-];
+// const gData = [
+//   { title: '0-0', key: '0-0' },
+//   { title: '0-1', key: '0-1' },
+//   { title: '0-2', key: '0-2', children: [{ title: '0-2-0', key: '0-2-0' }] },
+// ];
 
 class Demo extends React.Component {
   state = {
     gData,
     autoExpandParent: true,
-    // expandedKeys: ['0-0-key', '0-0-0-key', '0-0-0-0-key'],
-    expandedKeys: [],
+    expandedKeys: ['0-0-key', '0-0-0-key', '0-0-0-0-key'],
   };
 
   onDragEnter = ({ expandedKeys }) => {
@@ -48,96 +48,67 @@ class Demo extends React.Component {
     });
   };
 
-  onDrop = ({ node, dragNode, dropPosition: oriDropPosition, dropToGap }) => {
-    const { expandedKeys } = this.state;
+  onDrop = info => {
+    console.log('drop', info);
+    const dropKey = info.node.props.eventKey;
+    const dragKey = info.dragNode.props.eventKey;
+    const dropPos = info.node.props.pos.split('-');
+    const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
 
-    console.log('drop:', { node, dragNode, oriDropPosition, dropToGap });
-    const dropKey = node.props.eventKey;
-    const dragKey = dragNode.props.eventKey;
-    const dropPos = node.props.pos.split('-');
-    const dropPosition = oriDropPosition - Number(dropPos[dropPos.length - 1]);
-
-    // const dragDataNode = {};
-
-    // function dig(data) {
-    //   const newData = [...data];
-    //   let matchDrag = false;
-
-    //   for (let i = 0; i < newData.length; i += 1) {
-    //     const item = newData[i];
-    //     const cloneItem = {
-    //       ...item,
-    //       children: [...(item.children || [])],
-    //     };
-    //     newData[i] = cloneItem;
-
-    //     if (item.key === dragKey) {
-    //       // Is drag node
-    //       if (!matchDrag) {
-    //         matchDrag = true;
-    //         newData.splice(i, 1);
-    //         Object.assign(dragDataNode, cloneItem);
-    //         i -= 1;
-    //       }
-    //     } else if (item.key === dropKey) {
-    //       // Is drop node
-    //       if (!dropToGap) {
-    //         // Insert into children
-    //         cloneItem.children.push(dragDataNode);
-    //       } else if (dropPosition === -1) {
-    //         // Drop to the top
-    //         newData.splice(i, 0, dragDataNode);
-    //         i += 1;
-    //       } else {
-    //         // Drop to the bottom
-    //         newData.splice(i + 1, 0, dragDataNode);
-    //         i += 1;
-    //       }
-    //       // } else {
-    //       //   // Normal node should loop in
-    //       //   cloneItem.children = dig(cloneItem.children);
-    //     }
-
-    //     cloneItem.children = dig(cloneItem.children);
-    //   }
-
-    //   return newData;
-    // }
-
-    const { gData } = this.state;
-
-    function dig(list, callback) {
-      (list || []).forEach(item => {
-        callback(item);
-        dig(item.children);
+    const loop = (data, key, callback) => {
+      data.forEach((item, index, arr) => {
+        if (item.key === key) {
+          callback(item, index, arr);
+          return;
+        }
+        if (item.children) {
+          loop(item.children, key, callback);
+        }
       });
+    };
+    const data = [...this.state.gData];
+
+    // Find dragObject
+    let dragObj;
+    loop(data, dragKey, (item, index, arr) => {
+      arr.splice(index, 1);
+      dragObj = item;
+    });
+
+    if (!info.dropToGap) {
+      // Drop on the content
+      loop(data, dropKey, item => {
+        item.children = item.children || [];
+        // where to insert 示例添加到尾部，可以是随意位置
+        item.children.push(dragObj);
+      });
+    } else if (
+      (info.node.props.children || []).length > 0 && // Has children
+      info.node.props.expanded && // Is expanded
+      dropPosition === 1 // On the bottom gap
+    ) {
+      loop(data, dropKey, item => {
+        item.children = item.children || [];
+        // where to insert 示例添加到尾部，可以是随意位置
+        item.children.unshift(dragObj);
+      });
+    } else {
+      // Drop on the gap
+      let ar;
+      let i;
+      loop(data, dropKey, (item, index, arr) => {
+        ar = arr;
+        i = index;
+      });
+      if (dropPosition === -1) {
+        ar.splice(i, 0, dragObj);
+      } else {
+        ar.splice(i + 1, 0, dragObj);
+      }
     }
 
-    // Find drag node
-    let dragItem;
-    dig(gData, item => {
-      if (item.key === dragKey) {
-        dragNode = item;
-      }
-    });
-
-    // Find drop node
-    let dropItem;
-    dig(gData, item => {
-      if (dropToGap) {
-        // Match children exist
-      } else if (item.key === dropKey) {
-        dropItem = item;
-      }
-    });
-
-    const newData = dig(this.state.gData);
-    console.log('~~~>', newData);
-    console.log('EXT:', expandedKeys, dropKey);
-
     this.setState({
-      gData: newData,
-      expandedKeys: Array.from(new Set([...expandedKeys, dropKey])),
+      gData: data,
     });
   };
 
