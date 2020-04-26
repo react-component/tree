4b9ecd52a0f38d17306e1d5c27dfbef14a3040da
Anatoliy Zaslavskiy <toli@tolicodes.com>
2020-04-26 09:52:48 +0800

    Added checkbox stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,4 @@
 module.exports = {
-  stories: ['../src/stories/**/*.stories.js'],
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
-      options: {
-        presets: [['react-app', { flow: false, typescript: true }]],
-      },
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ['../src/stories/**/*.stories.(mdx|js)'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/preset-typescript', '@storybook/addon-docs'],
 };
