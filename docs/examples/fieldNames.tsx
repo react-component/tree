b4c9e7bff1bf9c3e6e19c6e4e7eaeb95794998d2
zombiej <smith3816@gmail.com>
2021-10-25 16:57:08 +0800

    fix: fieldNames checked logic

--- docs/examples/fieldNames.tsx
+++ docs/examples/fieldNames.tsx
@@ -4,36 +4,67 @@ import '../../assets/index.less';
 import './basic.less';
 import Tree from 'rc-tree';
 
-export default () => {
+const treeData = [
+  {
+    name: 'parent 1',
+    test: '0-0',
+    child: [
+      {
+        name: '张晨成',
+        test: '0-0-0',
+        disabled: true,
+        child: [
+          {
+            name: 'leaf',
+            test: '0-0-0-0',
+            disableCheckbox: true,
+          },
+          {
+            name: 'leaf',
+            test: '0-0-0-1',
+          },
+        ],
+      },
+      {
+        name: 'parent 1-1',
+        test: '0-0-1',
+        child: [
+          {
+            test: '0-0-1-0',
+            name: 'zcvc',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const Demo = () => {
+  const onSelect = (selectedKeys, info) => {
+    console.log('selected', selectedKeys, info);
+  };
+
+  const onCheck = (checkedKeys, info) => {
+    console.log('onCheck', checkedKeys, info);
+  };
+  const fieldNames = {
+    children: 'child',
+    title: 'name',
+    key: 'test',
+  };
+
   return (
     <Tree
-      // treeData={
-      //   [
-      //     {
-      //       fieldLabel: 'Root',
-      //       fieldKey: 'root',
-      //       fieldChildren: [
-      //         {
-      //           fieldLabel: 'Bamboo',
-      //           fieldKey: 'bamboo',
-      //         },
-      //         {
-      //           fieldLabel: 'Light',
-      //           fieldKey: 'light',
-      //         },
-      //       ],
-      //     },
-      //   ] as any
-      // }
-
-      // fieldNames={{
-      //   title: 'fieldLabel',
-      //   key: 'fieldKey',
-      //   children: 'fieldChildren',
-      // }}
-      defaultExpandAll
-    >
-      <Tree.TreeNode title="Bamboo" key="light" />
-    </Tree>
+      checkable
+      fieldNames={fieldNames}
+      defaultExpandedKeys={['0-0-0', '0-0-1']}
+      defaultSelectedKeys={['0-0-0', '0-0-1']}
+      defaultCheckedKeys={['0-0-0', '0-0-1']}
+      onSelect={onSelect}
+      onCheck={onCheck}
+      treeData={treeData as any}
+    />
   );
 };
+
+export default Demo;
