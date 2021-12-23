f9f55dec40dd14216434f8e47539c9289eeccc71
Haniel Cui <coldice945@hotmail.com>
2021-12-23 22:37:10 +0800

    fix: If set selectedKeys or expandedKeys to undefined, will be error (#492)
    Enhanced code robustness

--- src/util.tsx
+++ src/util.tsx
@@ -19,6 +19,7 @@ import {
 import { TreeProps, AllowDrop } from './Tree';
 
 export function arrDel(list: Key[], value: Key) {
+  if (!list) return [];
   const clone = list.slice();
   const index = clone.indexOf(value);
   if (index >= 0) {
@@ -28,7 +29,7 @@ export function arrDel(list: Key[], value: Key) {
 }
 
 export function arrAdd(list: Key[], value: Key) {
-  const clone = list.slice();
+  const clone = (list || []).slice();
   if (clone.indexOf(value) === -1) {
     clone.push(value);
   }
