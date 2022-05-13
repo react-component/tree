8661cedd6c40d4a65807902b4b092ca6b6fbf176
zombiej <smith3816@gmail.com>
2022-05-13 11:55:40 +0800

    test: more test

--- tests/React18.spec.tsx
+++ tests/React18.spec.tsx
@@ -3,7 +3,7 @@ import { render, fireEvent } from '@testing-library/react';
 import Tree from '../src';
 
 describe('React 18', () => {
-  it('not block in StrictMode', () => {
+  it('expand work', () => {
     const onExpand = jest.fn();
     const { container } = render(
       <Tree
@@ -25,14 +25,40 @@ describe('React 18', () => {
     );
 
     // All opened
+    expect(onExpand).not.toHaveBeenCalled();
     expect(
       container.querySelector('.rc-tree-list-holder').querySelectorAll('.rc-tree-treenode'),
     ).toHaveLength(2);
 
     // Collapse one
     fireEvent.click(container.querySelector('.rc-tree-switcher_open'));
+    expect(onExpand).toHaveBeenCalled();
     expect(
       container.querySelector('.rc-tree-list-holder').querySelectorAll('.rc-tree-treenode'),
     ).toHaveLength(1);
+  });
+
+  it('checkable work', () => {
+    const onCheck = jest.fn();
+    const { container } = render(
+      <Tree
+        defaultExpandAll
+        onCheck={onCheck}
+        checkable
+        treeData={[
+          {
+            title: 'Parent',
+            key: 'parent',
+          },
+        ]}
+      />,
+    );
+
+    expect(onCheck).not.toHaveBeenCalled();
+    expect(container.querySelector('.rc-tree-checkbox-checked')).toBeFalsy();
+
+    fireEvent.click(container.querySelector('.rc-tree-checkbox'));
+    expect(onCheck).toHaveBeenCalled();
+    expect(container.querySelector('.rc-tree-checkbox-checked')).toBeTruthy();
   });
 });
