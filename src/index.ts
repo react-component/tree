074f17f8b62a29d71dcdad664b308eb724658a76
高艳兵 <yanbing.gao@aliyun.com>
2026-05-25 13:29:45 +0800

    fix: trim tree type exports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import Tree from './Tree';
 import TreeNode from './TreeNode';
-import type { ExpandAction, TreeProps } from './Tree';
+import type { TreeProps } from './Tree';
 import type {
   BasicDataNode,
-  DataEntity,
   DataNode,
   EventDataNode,
   FieldDataNode,
-  FieldNames,
   GetCheckDisabled,
-  IconType,
-  Key,
-  SafeKey,
-  ScrollTo,
   TreeNodeProps,
 } from './interface';
 import { UnstableContext } from './contextTypes';
@@ -21,20 +15,6 @@ export { arrAdd, arrDel, conductExpandParent } from './util';
 export { conductCheck } from './utils/conductUtil';
 export { convertDataToEntities, convertTreeToData, fillFieldNames } from './utils/treeUtil';
 export { TreeNode, UnstableContext };
-export type {
-  BasicDataNode,
-  DataEntity,
-  DataNode,
-  EventDataNode,
-  ExpandAction,
-  FieldDataNode,
-  FieldNames,
-  GetCheckDisabled,
-  IconType,
-  Key,
-  SafeKey,
-  ScrollTo,
-  TreeNodeProps,
-  TreeProps,
-};
+export type { DataNode, EventDataNode, GetCheckDisabled };
+export type { TreeProps, TreeNodeProps, BasicDataNode, FieldDataNode };
 export default Tree;
